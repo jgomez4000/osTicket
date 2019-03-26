5e999593e32fa394a5cb4c564747a9e0d770de9f
Juan Andrés Gómez <jgomez4000@gmail.com>
2019-03-26 10:05:13 -0300

    Uno dos patches

--- include/upgrader/streams/core/9143a511-0d6099a6.patch.sql
+++ include/upgrader/streams/core/9143a511-0d6099a6.patch.sql
@@ -109,6 +109,22 @@ UPDATE `%TABLE_PREFIX%config` SET `value` = '1'
 ALTER TABLE `%TABLE_PREFIX%attachment`
   ADD `name` varchar(255) NULL default NULL AFTER `file_id`;
 
+-- Patch to add support for Organization restrictions
+-- for help topics. This table provides the necessary 
+-- many to many relationships needed for this feature.
+CREATE TABLE `%TABLE_PREFIX%help_topic_organization` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `topic_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `organization_id` int(11) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `topic-organization` (`topic_id`,`organization_id`)
+) DEFAULT CHARSET=utf8;
+
+-- Adds the ability to restrict help topics to only the
+-- primary contact of an organization. This is used for
+-- allowing only managers, etc. access to certain topics.
+ALTER TABLE `%TABLE_PREFIX%help_topic` ADD COLUMN orgpconly tinyint(1) unsigned NOT NULL DEFAULT '0' AFTER ispublic;
+
 -- Finished with patch
 UPDATE `%TABLE_PREFIX%config`
     SET `value` = '0d6099a650cc7884eb59a040feab2ce8'
